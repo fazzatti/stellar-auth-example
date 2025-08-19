5a01d396de1b2573bc2e033c726413fb618cc90b
Fabricius Zatti <fazzatti@gmail.com>
2025-08-19 10:40:40 -0300

    feat: send transaction

---
+++ src/send-transaction.ts
@@ -0,0 +1,45 @@
+import { Api } from "stellar-sdk/rpc";
+import { args, config, rpc } from "./config/env.ts";
+import { TransactionBuilder } from "stellar-sdk";
+
+const { stellarNetwork } = config;
+const { tx } = args;
+
+if (!tx) {
+  throw new Error("Transaction XDR (--tx) is required");
+}
+
+const transaction = TransactionBuilder.fromXDR(tx, stellarNetwork);
+
+console.log("Submitting transaction...");
+
+rpc
+  .sendTransaction(transaction)
+  .then(async (reply) => {
+    if (reply.status !== "PENDING") {
+      throw reply;
+    }
+
+    return rpc.pollTransaction(reply.hash, {
+      sleepStrategy: (_iter: number) => 1000,
+      attempts: 5,
+    });
+  })
+  .then((finalStatus) => {
+    switch (finalStatus.status) {
+      case Api.GetTransactionStatus.FAILED:
+      case Api.GetTransactionStatus.NOT_FOUND:
+        throw new Error(
+          `Transaction failed with status: ${finalStatus.status}`
+        );
+      case Api.GetTransactionStatus.SUCCESS:
+        console.log("Transaction succeeded:", finalStatus.txHash);
+        return finalStatus.status;
+    }
+  })
+  .then(function (result) {
+    console.log("Success! Results:", result);
+  })
+  .catch(function (error) {
+    console.error("Something went wrong!", error);
+  });
